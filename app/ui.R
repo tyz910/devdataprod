e572753ae98b11e0dc84a197c59e670a3acfee07
Evgeniy Ivanov <tyz910@gmail.com>
2015-06-16 16:45:44 +0300

    init

---
+++ app/ui.R
@@ -0,0 +1,27 @@
+# library(shinyapps)
+# shinyapps::deployApp('app', appName='courcera')
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
